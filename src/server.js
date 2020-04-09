7f06d5b72c2729fad6ff6cb4dfcf0ddfad0e4520
wagner nascimento <wagneer_nascimento@live.com>
2020-04-08 21:20:47 -0300

    instalação do cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 require('./database/index');
 const app = express();
+
+
 app.use(express.json());
 app.use(routes);
-
+app.use(cors());
 app.listen(3003);
