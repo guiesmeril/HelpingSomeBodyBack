46662bbbb3437d4f73f1ad9875c455f1a07c90a7
wagner nascimento <wagneer_nascimento@live.com>
2020-04-08 21:46:06 -0300

    adicionando body parser para trabalhar com json

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const routes = require('./routes');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 require('./database/index');
 const app = express();
 
-
-app.use(express.json());
-app.use(routes);
-app.use(cors());
+app.use(bodyParser.json());
+app.use(cors()); 
+app.use('/', require("./routes"));
 app.listen(3003);
