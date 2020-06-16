d2103059540882852289b1dd646767009e86b8eb
guiesmeril <guiesmeril@hotmail.com>
2020-06-16 20:07:23 -0300

    Atualizando o back-end, colocando para verificar se e o e-mail informado no cadastro já existe na base de dados

--- src/controls/VoluntarioController.js
+++ src/controls/VoluntarioController.js
@@ -1,10 +1,18 @@
 const Voluntario = require('../models/Voluntario');
+const { Op } = require("sequelize");
 
 module.exports = {
-
     async create(req , res){
         console.log(req.body);
         try {
+            const usuario = await Voluntario.findOne({where:{
+                email: req.body.email
+            }})
+
+            if(usuario) {
+                res.status(401).json({error: 'usuario ja existe'})
+            }
+
            const voluntario = await Voluntario.create(req.body);
             res.status(200).json(voluntario);
 
@@ -12,5 +20,8 @@ module.exports = {
             return err;
         }
     }
+
+
+
 }
 
